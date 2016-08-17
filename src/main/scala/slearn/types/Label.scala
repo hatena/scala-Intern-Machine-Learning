661c52d4be43aa2d7cc79430d97e11f00795db26
tarao <tarao.gnn@gmail.com>
2016-08-17 17:54:26 +0900

    Return if the label is positive or negative.

--- src/main/scala/slearn/types/Label.scala
+++ src/main/scala/slearn/types/Label.scala
@@ -3,6 +3,8 @@ package types
 
 sealed abstract class Label(value: Int) {
   def toInt(): Int = value
+  def positive: Boolean = value > 0
+  def negative: Boolean = value < 0
 }
 object Label {
   case object Positive extends Label(1)
